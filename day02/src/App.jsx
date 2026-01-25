8d83db54a00f110b3572e55158c15786634b9b47
change <you@example.com>
2026-01-25 12:08:30 +0530

    upload day 1 and 2 file to github

---
+++ day02/src/App.jsx
@@ -0,0 +1,15 @@
+import Classcom from "./components/Classcom";
+import Func from "./components/Func";
+
+function  App(){
+  return (
+
+    <>
+   
+    <Func name="Nitin" age={25} />
+    
+    </>
+  )
+}
+
+export default App;
---
+++ day02/src/components/Classcom.jsx
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+
+
+class Classcom extends React.Component {
+  render() {
+    return (<div>Class Component</div>);
+    }
+}
+export default Classcom;
